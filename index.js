c4f46d25d695f2835823ec34359e206216ad2b3e
Ashwin746544 <ahpatel99999@gmail.com>
2022-02-21 18:36:49 -0800

    fix: complete ToDoList app

--- index.js
+++ index.js
@@ -3,49 +3,53 @@ const addBtn = document.querySelector(".addBtn");
 const saveBtn = document.querySelector(".saveBtn");
 const titleInput = document.querySelector("#title");
 const discriptionInput = document.querySelector("#disc");
-let CheckboxArray = [];
+let CheckboxArray = JSON.parse(localStorage.getItem("CheckboxArray")) || [];
 let tasksArray = JSON.parse(localStorage.getItem("userTasks"));
+
+
 if (tasksArray == null) {
   tasksArray = [];
   localStorage.setItem("userTasks", JSON.stringify(tasksArray));
 }
-// if (tasksArray.length == 0) {
-//   document.querySelector('.no-task').classList.toggle("enableNoTasks");
-// } else {
-let taskshtml = "";
-tasksArray.forEach((value, index, array) => {
-  taskshtml += `<div class="task" id="task-${value.taskId}">
+if (tasksArray.length == 0) {
+  document.querySelector('.no-task').style.display = "flex";
+} else {
+  let taskshtml = "";
+  tasksArray.forEach((value, index, array) => {
+    taskshtml += `<div class="task" id="task-${value.taskId}">
     <div class="check-container">
-      <input type="checkbox" name="isChecked" id="checkItem-${value.taskId}" onclick="onTaskSelect(this)">
-    </div>
-    <div class="content">
+      <input type="checkbox" name="isChecked" id="checkItem-${value.taskId}" onclick="onTaskSelect(this)"  ${CheckboxArray.includes("checkItem-" + value.taskId) ? "checked" : ""}>
+    </div >
+      <div class="content ${CheckboxArray.includes("checkItem-" + value.taskId) ? "addBackground" : ""}">
       <h2 class="content-title" id="title-${value.taskId}">${value.title}</h2>
       <p class="content-discription" id="disc-${value.taskId}">
       ${value.discription}
       </p>
-    </div>
-    <div class="actions">
-      <button class="edit" id="edit-${value.taskId}" onclick="editTask(this.id)"><img src="./assets/icons8-edit.svg" alt="Edit"></button>
-      <button class="delete" id="delete-${value.taskId}" onclick="deleteTask(this.id)"><img src="./assets/icons8-delete.svg" alt="Delete"></button>
-    </div>
-  </div>`;
-});
-Tasks_container.innerHTML = taskshtml;
-// }
+    </div >
+      <div class="actions">
+        <button class="edit" id="edit-${value.taskId}" onclick="editTask(this.id)"><img src="./assets/icons8-edit.svg" alt="Edit"></button>
+        <button class="delete" id="delete-${value.taskId}" onclick="deleteTask(this.id)"><img src="./assets/icons8-delete.svg" alt="Delete"></button>
+      </div>
+  </div > `;
+  });
+  document.querySelector('.no-task').style.display = "none";
+  Tasks_container.innerHTML = taskshtml;
 
-function enableDefaultTask() {
-  document.querySelector('.no-task').styel.display = "block";
 }
+// Create Task
 addBtn.addEventListener("click", addTask);
+addBtn.addEventListener("click", () => {
+  // addBtn.style.animation = "btnAnimate 20s";
+})
 function addTask() {
-  const title = titleInput.value;
-  const discription = discriptionInput.value;
+  console.log('onclick fired');
+  const title = titleInput.value.trim();
+  const discription = discriptionInput.value.trim();
   if (title == "" || discription == "") {
     return;
   }
-  document.forms["myForm"].reset();
+  // document.forms["myForm"].reset();
   const taskId = new Date().getTime();
-  console.log(title + discription);
   let newTask = { title: title, discription: discription, taskId: taskId };
   tasksArray.unshift(newTask);
   localStorage.setItem("userTasks", JSON.stringify(tasksArray));
@@ -63,12 +67,15 @@ function addTask() {
   <button class="edit" id="edit-${taskId}" onclick="editTask(this.id)"><img src="./assets/icons8-edit.svg" alt="Edit"></button>
   <button class="delete" id="delete-${taskId}" onclick="deleteTask(this.id)"><img src="./assets/icons8-delete.svg" alt="Delete"></button>
 </div>`;
-  console.log(task);
   const FirstChild = Tasks_container.firstChild;
-  console.log("this is first child", FirstChild);
   Tasks_container.insertBefore(task, FirstChild);
+  if (tasksArray.length == 1) {
+    document.querySelector('.no-task').style.display = "none";
+  }
+  return false;
 }
 
+//Delete Task
 function deleteTask(btnId) {
   if (CheckboxArray.length >= 2) {
     CheckboxArray.forEach((checkboxId) => {
@@ -82,11 +89,8 @@ function deleteTask(btnId) {
     CheckboxArray = [];
     localStorage.setItem("userTasks", JSON.stringify(tasksArray));
   } else {
-    console.log("delete");
     const id = btnId.substring(7);
-    console.log(id);
     const taskToDelete = document.querySelector("#task-" + id);
-    console.log(taskToDelete);
     taskToDelete.remove();
     const indexToDelete = tasksArray.findIndex((value, index, array) => {
       return value.taskId == id;
@@ -95,48 +99,66 @@ function deleteTask(btnId) {
     localStorage.setItem("userTasks", JSON.stringify(tasksArray));
     CheckboxArray = [];
   }
+  if (tasksArray.length == 0) {
+    document.querySelector('.no-task').style.display = "flex";
+  } else {
+    document.querySelector('.no-task').style.display = "none";
+  }
 }
+
+// Edit Task
 function editTask(editId) {
   const allEditBtns = document.querySelectorAll(".edit");
   Array.from(allEditBtns).forEach((element) => { element.style.cursor = "not-allowed"; element.style.pointerEvents = "none"; });
-  console.log("edit");
   const id = editId.substring(5);
-  console.log(id);
+
   const editTitle = document.querySelector("#title-" + id).innerText;
   const editDisc = document.querySelector("#disc-" + id).innerText;
-  console.log(editTitle);
-  console.log(editDisc);
+
   titleInput.value = editTitle;
   discriptionInput.value = editDisc;
-  const content = document.querySelector(`#${editId}`).parentNode.previousElementSibling;
-  console.log(content);
+  const content = document.querySelector(`#${editId} `).parentNode.previousElementSibling;
+
   content.classList.toggle("addBackground");
-  addBtn.style.display = "none";
-  saveBtn.style.display = "block";
+  // addBtn.style.display = "none";
+  // saveBtn.style.display = "block";
+  addBtn.value = "Save Task";
+  addBtn.style.backgroundColor = "green";
   const f = () => saveTask(id);
-  saveBtn.addEventListener("click", f);
+  // saveBtn.addEventListener("click", f);
+  addBtn.removeEventListener("click", addTask);
+  addBtn.addEventListener("click", f);
+
+  // save Task
   function saveTask(id) {
-    console.log("saveid" + id);
     const title = titleInput.value;
     const discription = discriptionInput.value;
     if (title == "" || discription == "") {
-      document.forms["myForm"].reset();
+      // document.forms["myForm"].reset();
       content.classList.toggle("addBackground");
-      addBtn.style.display = "block";
-      saveBtn.style.display = "none";
+      // addBtn.style.display = "block";
+      // saveBtn.style.display = "none";
+      addBtn.value = "Add Task";
+      addBtn.style.backgroundColor = "purple";
+      addBtn.addEventListener("click", addTask);
+      Array.from(allEditBtns).forEach((element) => { element.style.cursor = "pointer"; element.style.pointerEvents = "auto"; });
       return;
     }
     document.querySelector("#title-" + id).innerHTML = title;
     document.querySelector("#disc-" + id).innerHTML = discription;
-    document.forms["myForm"].reset();
-    addBtn.style.display = "block";
-    saveBtn.style.display = "none";
+    // document.forms["myForm"].reset();
+    // addBtn.style.display = "block";
+    // saveBtn.style.display = "none";  
+    addBtn.value = "Add Task";
+    addBtn.style.backgroundColor = "purple";
     tasksArray = tasksArray.map((value, index, array) => {
       return value.taskId == id ? { taskId: id, title: title, discription: discription } : value;
     }
     );
     localStorage.setItem("userTasks", JSON.stringify(tasksArray));
-    saveBtn.removeEventListener("click", f);
+    // saveBtn.removeEventListener("click", f);
+    addBtn.removeEventListener("click", f);
+    addBtn.addEventListener("click", addTask);
     content.classList.remove("addBackground");
     Array.from(allEditBtns).forEach((element) => { element.style.cursor = "pointer"; element.style.pointerEvents = "auto"; });
   }
@@ -147,12 +169,17 @@ function onTaskSelect(checkbox) {
     checkbox.parentNode.nextElementSibling.classList.toggle("addBackground");
     checkbox.parentNode.nextElementSibling.style.textDecoration = "line-through";
     CheckboxArray.push(checkbox.id);
-    console.log(CheckboxArray);
+
   } else {
     checkbox.parentNode.nextElementSibling.classList.toggle("addBackground");
     checkbox.parentNode.nextElementSibling.style.textDecoration = "none";
     const index = CheckboxArray.indexOf(checkbox.id);
     CheckboxArray.splice(index, 1);
-    console.log(CheckboxArray);
+
   }
-};
+}
+
+window.onbeforeunload = () => {
+  localStorage.setItem("CheckboxArray", JSON.stringify(CheckboxArray));
+  console.log("onunload");
+}
